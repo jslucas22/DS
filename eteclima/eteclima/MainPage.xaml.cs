e3c50d24d9526b09a5b70b92eb1bc6c93cf75f64
manager <manager@LAB3MAQ19>
2019-06-12 22:29:31 -0300

    commit principal

---
+++ eteclima/eteclima/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using eteclima.Model;
+using eteclima.Services;
+
+namespace eteclima
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Previsão do Tempo";
+            //define o databinding para os objetos padrão iniciais
+            this.BindingContext = new Tempo();
+        }
+
+        private async void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidadeEntry.Text))
+                {
+                    Tempo previsaoDoTempo = await DataService.GetPrevisaoDoTempo(cidadeEntry.Text);
+                    this.BindingContext = previsaoDoTempo;
+                    btnPrevisao.Text = "Nova Previsao";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro ", ex.Message, "OK");
+            }
+        }
+    }
+}
